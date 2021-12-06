1e5221355e34a0944b1e611cbe7c4cdfa953d9be
Fernando <augustfernando777@gmail.com>
2021-12-06 13:43:58 +0700

    i2c keypad PLN

--- MQTT_Telegram_main/keypadConfig.h
+++ MQTT_Telegram_main/keypadConfig.h
@@ -7,12 +7,15 @@
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'},
-    {'*', '0', '#'}};
-byte rowPins[ROWS] = {2, 7, 6, 4}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {3, 1, 5};
+    {'1', '2', '3'},//4
+    {'4', '5', '6'},//3
+    {'7', '8', '9'},//2
+    {'*', '0', '#'}};//1
+    //7    6    5
+//byte rowPins[ROWS] = {2, 7, 6, 4}; //connect to the row pinouts of the keypad
+//byte colPins[COLS] = {3, 1, 5};
+byte rowPins[ROWS] = {4, 3, 2, 1}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7, 6, 5};
 String allkey = "";
 bool checked = false;
 Keypad_I2C keypad = Keypad_I2C(makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR);
